a4619fbfc1ebc357e5bfa635fcd05782f1d8a1a5
qjh <zs@bjpowernode.com>
2020-11-30 16:30:06 +0800

    dad

--- src/main/java/com/entor/controller/HelloController.java
+++ src/main/java/com/entor/controller/HelloController.java
@@ -1,12 +1,21 @@
 package com.entor.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${server.port}")
+    private  int port;
     @GetMapping("/hello")
     public String hello(){
         return "hello git";
     }
+
+    @GetMapping("/get")
+    public String get(){
+        return "服务器端口:"+port;
+    }
+
 }
